ee38a9c5fd8e617924c3c3d49153693194ea847f
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-08 22:48:31 +0400

    move replacer into a separate module

---
+++ modules/replacer.js
@@ -0,0 +1,17 @@
+function replacer(action, shift, match) {
+    if (action === 'encode' && shift >= 0 || action === 'decode' && shift <= 0) {
+        shift = Math.abs(shift);
+        if (match.toLowerCase().charCodeAt(0) + shift > 'z'.charCodeAt(0)) {
+            shift = shift - 26;
+        }
+    }
+    if (action === 'encode' && shift <= 0 || action === 'decode' && shift >= 0) {
+        shift = -Math.abs(shift);
+        if (match.toLowerCase().charCodeAt(0) + shift < 'a'.charCodeAt(0)) {
+            shift = shift + 26;
+        }
+    }
+    return String.fromCharCode(match.charCodeAt(0) + shift);
+}
+
+module.exports.replacer = replacer;
--- modules/rtw.js
+++ modules/rtw.js
@@ -3,21 +3,7 @@ const fs = require('fs');
 const { pipeline } = require('stream');
 const stream = require('stream');
 
-function replacer(action, shift, match) {
-    if (action === 'encode' && shift >= 0 || action === 'decode' && shift <= 0) {
-        shift = Math.abs(shift);
-        if (match.toLowerCase().charCodeAt(0) + shift > 'z'.charCodeAt(0)) {
-            shift = shift - 26;
-        }
-    }
-    if (action === 'encode' && shift <= 0 || action === 'decode' && shift >= 0) {
-        shift = -Math.abs(shift);
-        if (match.toLowerCase().charCodeAt(0) + shift < 'a'.charCodeAt(0)) {
-            shift = shift + 26;
-        }
-    }
-    return String.fromCharCode(match.charCodeAt(0) + shift);
-}
+const { replacer } = require('./replacer.js');
 
 const transform = new stream.Transform({
     transform: function (chunk, _, callback) {
