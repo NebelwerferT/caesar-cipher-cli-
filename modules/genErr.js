362cac2d510a7cf403c64cc6bc34f7033cbfd58a
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-09 00:57:45 +0400

    update genErr.js

--- modules/genErr.js
+++ modules/genErr.js
@@ -1,14 +1,27 @@
+const os = require('os');
+
 function genErr(errMsgTmps) {
-    if (errMsgTmps.input === 'error' && errMsgTmps.output === 'error') {
-        process.stderr.write('error: input and output files do not exist');
-        process.exit(1);
+    let errMsgs = [];
+    if (errMsgTmps.input === 'exists error' && errMsgTmps.output === 'exists error') {
+        errMsgs.push('error: input and output files do not exist');
     }
-    if (errMsgTmps.input === 'error') {
-        process.stderr.write('error: input file does not exist');
-        process.exit(1);
+    else if (errMsgTmps.input === 'exists error') {
+        errMsgs.push('error: input file does not exist');
     }
-    if (errMsgTmps.output === 'error') {
-        process.stderr.write('error: output file does not exist');
+    else if (errMsgTmps.output === 'exists error') {
+        errMsgs.push('error: output file does not exist');
+    }
+    if (errMsgTmps.input === 'access error' && errMsgTmps.output === 'access error') {
+        errMsgs.push('error: input and output files exist but are not accessible');
+    }
+    else if (errMsgTmps.input === 'access error') {
+        errMsgs.push('error: input file exists but is not readable');
+    }
+    else if (errMsgTmps.output === 'access error') {
+        errMsgs.push('error: output file exists but is not writable');
+    }
+    if (errMsgs.length) {
+        process.stderr.write(errMsgs.join(os.EOL));
         process.exit(1);
     }
 }
