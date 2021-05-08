d71c28f62a5f5f92b8caa0852a6ea489880842fd
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-08 22:13:53 +0400

    create: checkPaths.js

---
+++ modules/checkPaths.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+function fileExists(path) {
+    if (fs.existsSync(path)) {
+        return 'succes';
+    }
+    else {
+        return 'error';
+    }
+
+}
+
+function genErr(errMsgTmps) {
+    if (errMsgTmps.input === 'error' && errMsgTmps.output === 'error') {
+        process.stderr.write('error: input and output files do not exist');
+        process.exit(1);
+    }
+    if (errMsgTmps.input === 'error') {
+        process.stderr.write('error: input file does not exist');
+        process.exit(1);
+    }
+    if (errMsgTmps.output === 'error') {
+        process.stderr.write('error: output file does not exist');
+        process.exit(1);
+    }
+}
+
+function checkPaths(paths) {
+    let errMsgTmps = {};
+
+    for (let path in paths) {
+        if (paths[path] !== undefined) {
+            errMsgTmps[path] = (fileExists(paths[path]));
+        }
+    }
+    genErr(errMsgTmps);
+
+    return paths;
+}
+
+module.exports.checkPaths = checkPaths;
